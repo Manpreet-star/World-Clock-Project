4ed6445b5b9c02bc9978f51f548b385c2836ae31
Manpreet-star <manu919191@gmail.com>
2025-03-29 00:06:51 +0000

    added current location option

--- src/index.js
+++ src/index.js
@@ -36,11 +36,14 @@ setInterval(updateTime, 1000);
 
 function updateCity(event) {
   let cityTimeZone = event.target.value;
+  if (cityTimeZone === "current") {
+    cityTimeZone = moment.tz.guess();
+  }
+
   let cityName = cityTimeZone.replace("_", " ").split("/")[1];
   let cityTime = moment().tz(cityTimeZone);
   let citiesElement = document.querySelector("#cities");
   citiesElement.innerHTML = `
- <div id="cities">
        <div class="city">
        <div><h2>${cityName}</h2>
         <div class="date">${cityTime.format("MMMM Do YYYY")}</div>
